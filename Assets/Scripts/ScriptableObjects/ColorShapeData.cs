9437001d2d73198b0732d6cc6ee1529baab5db50
YitssTG <ismaelhuaman300@gmail.com>
2025-05-13 01:00:01 -0500

    Scripts Set Up

---
+++ Assets/Scripts/ScriptableObjects/ColorShapeData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Color Shape Data", menuName = "Scriptable objects/Game 1/ColorShapeData")]
+public class ColorShapeData : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
